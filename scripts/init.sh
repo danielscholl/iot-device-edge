893fa86e3c2ff9937ed7af72890a56740199b10b
Daniel Scholl <daniel@scholllopez.com>
2019-02-22 14:20:08 -0600

    Fixed TTY errors

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,21 +6,21 @@
 
 # Move the Certificates
 printf "\n"
-tput setaf 2; echo "Moving the Certificates into place" ; tput sgr0
-tput setaf 3; echo "------------------------------------" ; tput sgr0
+tput -T xterm setaf 2; echo "Moving the Certificates into place" ; tput -T xterm sgr0
+tput -T xterm setaf 3; echo "------------------------------------" ; tput -T xterm sgr0
 sudo mv certs /etc/iotedge/
 
 # Rewrite the Configuration
 printf "\n"
-tput setaf 2; echo "Moving the Configuration into place" ; tput sgr0
-tput setaf 3; echo "------------------------------------" ; tput sgr0
+tput -T xterm setaf 2; echo "Moving the Configuration into place" ; tput -T xterm sgr0
+tput -T xterm setaf 3; echo "------------------------------------" ; tput -T xterm sgr0
 sudo cp /etc/iotedge/config.yaml /etc/iotedge/config_bu.yaml
 sudo mv config.yaml /etc/iotedge/
 
 # Rewrite the Configuration
 printf "\n"
-tput setaf 2; echo "Starting up IotEdge" ; tput sgr0
-tput setaf 3; echo "------------------------------------" ; tput sgr0
+tput -T xterm setaf 2; echo "Starting up IotEdge" ; tput -T xterm sgr0
+tput -T xterm setaf 3; echo "------------------------------------" ; tput -T xterm sgr0
 sudo systemctl stop iotedge
 sudo systemctl daemon-reload
 sudo systemctl start iotedge
